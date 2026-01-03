b387b734b39fc3b39e3b159d0b2d8ee29b3b06c8
MANOJ KUMAR SINGH <manojdsingh2000@yahoo.co.in>
2026-01-03 19:13:28 +0530

    Update server.py

--- server.py
+++ server.py
@@ -5,41 +5,7 @@ from flask_cors import CORS
 import os
 from worker import speech_to_text, text_to_speech, watsonx_process_message
 
-# To call watsonx's LLM, we need to import the library of IBM Watson Machine Learning
-from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
-from ibm_watson_machine_learning.foundation_models import Model
 
-# placeholder for Watsonx_API and Project_id incase you need to use the code outside this environment
-# API_KEY = "Your WatsonX API"
-PROJECT_ID= "skills-network"
-
-# Define the credentials 
-credentials = {
-    "url": "https://us-south.ml.cloud.ibm.com"
-    #"apikey": API_KEY
-}
-    
-# Specify model_id that will be used for inferencing
-model_id = "mistralai/mistral-medium-2505"
-
-
-# Define the model parameters
-from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
-from ibm_watson_machine_learning.foundation_models.utils.enums import DecodingMethods
-
-parameters = {
-    GenParams.DECODING_METHOD: DecodingMethods.GREEDY,
-    GenParams.MIN_NEW_TOKENS: 1,
-    GenParams.MAX_NEW_TOKENS: 1024
-}
-
-# Define the LLM
-model = Model(
-    model_id=model_id,
-    params=parameters,
-    credentials=credentials,
-    project_id=PROJECT_ID
-)
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
